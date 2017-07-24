464d3cc38a951033da638cd7aab90e181f2de72b
Murdho <murdho@murdho.com>
2017-07-24 14:27:38 +0300

    Introduce deep_dup

--- lib/rack/cargo/request_env_builder.rb
+++ lib/rack/cargo/request_env_builder.rb
@@ -1,8 +1,10 @@
+require "rack/cargo/core_ext/deep_dup"
+
 module Rack
   module Cargo
     module RequestEnvBuilder
       def self.call(request, state)
-        request_env = state.fetch(:env).dup # TODO: should be deep
+        request_env = state.fetch(:env).deep_dup
         request_env[ENV_PATH] = request[REQUEST_PATH]
         request_env[ENV_METHOD] = request[REQUEST_METHOD]
         request_env[ENV_INPUT] = StringIO.new(request[REQUEST_BODY].to_json)
